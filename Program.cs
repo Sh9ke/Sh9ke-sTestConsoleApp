4fc6945a3460e4bc03c628e05e5ef06266fc0f5b
Alexandr Repetsky <73669511+Sh9ke@users.noreply.github.com>
2022-12-05 20:15:09 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sh9ke_sTestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
